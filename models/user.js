317d10408e470f919e0125e1da95221f53c7f296
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-01-20 01:11:19 +0000

    Remove env

---
+++ models/user.js
@@ -0,0 +1,30 @@
+import { Schema, model, models } from 'mongoose'
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      'Username invalid, it should contain 8-20 alphanumeric letters and be unique!',
+    ],
+  },
+  image: {
+    type: String,
+  },
+})
+
+//in NextJs, there is no need to define the model every single time
+//like how it works in Express.
+//So, first check if the model already exists in the models object.
+//If it does, use that. Otherwise, create a new model.
+// this prevents it from being redefined every single time when the particular route gets called.
+
+const User = models.User || model('User', UserSchema)
+
+export default User
