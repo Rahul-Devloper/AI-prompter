75073570c43976f84acfb57db12b78bdf56616be
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-01-20 01:44:45 +0000

    implemented google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,10 @@
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
 
+import User from '@models/user'
+
+import { connectToDB } from '@utils/database'
+
 // console.log(process.env.GOOGLE_ID)
 const handler = NextAuth({
   providers: [
@@ -10,8 +14,44 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  callbacks: {
+    async session({ session }) {
+      //the argument session is the session object that is received from the google authentication provider
+      //to get information about the loggedIn user
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      })
+
+      session.user.id = sessionUser._id.toString()
+
+      return session
+    },
+    async signIn({ profile }) {
+      try {
+        // serverless function
+        await connectToDB()
+
+        // check if user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        })
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    },
+  },
 })
 
 export { handler as GET, handler as POST }
