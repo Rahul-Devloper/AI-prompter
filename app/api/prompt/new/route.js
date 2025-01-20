4876b2f68f315b852f31257764ab38b62c8495ba
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-01-20 16:53:37 +0000

    added Form and Prompt model

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,26 @@
+import Prompt from '@models/prompt'
+import { connectToDB } from '@utils/database'
+
+export const POST = async (req) => {
+  const { userId, prompt, tag } = await req.json()
+  console.log('prompt=>', prompt)
+
+  try {
+    await connectToDB()
+
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    })
+    console.log('newPrompt=>', newPrompt)
+    const savePrompt = await newPrompt.save()
+
+    console.log('savePrompt=>', savePrompt)
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 })
+  } catch (error) {
+    console.log('error', error)
+    return new Response('Failed to create a new prompt', { status: 500 })
+  }
+}
